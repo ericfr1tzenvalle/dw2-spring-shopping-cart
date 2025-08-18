481a15db69478fd77255886833c23bc3cc2f5f68
ericfr1tzenvalle <ericfritzenvalle@gmail.com>
2025-08-17 23:50:32 -0300

    feat(ProdutoController): implementar controller que lida com listarProdutos, enviar um produto vazio para form e salvar produtos.

--- src/main/java/com/ericfr1tzenvalle/shopping_cart/controller/ProdutoController.java
+++ src/main/java/com/ericfr1tzenvalle/shopping_cart/controller/ProdutoController.java
@@ -4,10 +4,53 @@
  */
 package com.ericfr1tzenvalle.shopping_cart.controller;
 
+import com.ericfr1tzenvalle.shopping_cart.model.Produto;
+import com.ericfr1tzenvalle.shopping_cart.repository.ProdutoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 /**
  *
  * @author Luísa
  */
+
+@Controller
 public class ProdutoController {
     
+    @Autowired
+    private ProdutoRepository repository;
+    
+    
+    @GetMapping("/produtos")
+    public String listarProdutos(Model model){
+        model.addAttribute("produtos", repository.getProdutos());
+        return "listar_produtos";
+        
+    }
+    
+    
+    @GetMapping("/produtos/novo")
+    public String novoProdutoForm(Model model){
+       model.addAttribute("produto", new Produto());
+       return "form-produto";
+    }
+    @PostMapping("/produtos")
+    public String salvarProduto(@Valid @ModelAttribute("produto") Produto produto, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "form-produto";
+        }
+        repository.salvarProduto(produto);
+        return "redirect:/produtos";
+    }
+    
+    
+    
+    
 }
+
